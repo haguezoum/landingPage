ce02b533c5a80cd5dfff4320b55c2b73243789b9
haguezoum <haguezoum@gmail.com>
2021-06-30 15:20:54 +0100

    custom fotter

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,21 +4,26 @@
             <!-- wave decoration :) -->
         </div>
 
-        <div class="content flex flex-col justify-center items-center border-2 border-yellow-300">
+        <div class="content flex flex-col justify-center items-center border-2 border-yellow-300 p-2">
             <!-- part 1 -->
-            <div class="header w-full titile border text-center">
+            <div class="header w-full titile text-center border border-green-300 mb-2">
                 <h3 class="text-3xl text-white font-bold py-6">What are you waiting for?</h3>
                     <button class="py-2  mt-6 px-6 uppercase text-white bg-green-300 bg-opacity-60 text-lg rounded-full hover:bg-green-700">Get started</button>
             </div>
 
             <!-- part 2 -->
-            <div class="content w-full">
-                <div class="img">
+            <div class="content w-full flex justify-between items-center border border-yellow-300 p-2">
+
+                <div class="img  flex justify-center items-center border-2 border-pink-400 mb-2">
                     <img src="/img/icone-3.png" alt="" srcset="">
+                    <p>Hello world</p>
                 </div>
-                <div class="">
+
+                <!-- ---------------------------------------------------------------- -->
+
+                <div class="justify-around items-center border border-blue-300 p-2">
                     <!-- sm:dropdown   lg:columns -->
-                    <el-dropdown>
+                    <!-- <el-dropdown>
                         <span class="el-dropdown-link">
                             Dropdown List<i class="el-icon-arrow-down el-icon--right"></i>
                         </span>
@@ -29,21 +34,41 @@
                             <el-dropdown-item disabled>Action 4</el-dropdown-item>
                             <el-dropdown-item divided>Action 5</el-dropdown-item>
                         </el-dropdown-menu>
-                    </el-dropdown>
+                    </el-dropdown> -->
+                    <div class="flex lfex-row">
+                            <div class="border " v-for="item,index in 3" :key="index">
+                            <div class="title w-full text-center">Hello</div>
+                            <div class="list w-full">
+                                <ul class="w-full flex flex-col justify-center">
+                                    <li>Lorem, ipsum dolor.</li>
+                                    <li>Lorem, ipsum dolor.</li>
+                                    <li>Lorem, ipsum dolor.</li>
+                                    <li>Lorem, ipsum dolor.</li>
+                                    <li>Lorem, ipsum dolor.</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- ************************ -->
                 </div>
-                <div>
+
+                <!-- ---------------------------------------------------------------- -->
+                
+                <div class=" flex flex-col">
                     <div class="social-media">
-                        <ul>
-                          <li><a href="">fb</a></li>
-                          <li><a href="">tw</a></li>
-                          <li><a href="">ig</a></li>
-                          <li><a href="">gg</a></li>
+                        <ul class="flex justify-around items-center">
+                          <li class="h-10 w-10 rounded-full border-green-400"><a href="">fb</a></li>
+                          <li class="h-10 w-10 rounded-full border-green-400"><a href="">tw</a></li>
+                          <li class="h-10 w-10 rounded-full border-green-400"><a href="">ig</a></li>
+                          <li class="h-10 w-10 rounded-full border-green-400"><a href="">gg</a></li>
                         </ul>
                     </div>
                     <div class="languages">
-                        <!-- dropdown -->
+                        <!-- dropdown --> <p class="text-3xl">test</p>
                     </div>
                 </div>
+                <!-- ---------------------------------------------------------------- -->
+
             </div>
 
 
@@ -52,6 +77,7 @@
 </template>
 
 <script>
+
 export default {
     name:'Footer',
 }
--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,7 +1,7 @@
 <template>
-  <main>
-    <Header class="bg-purple-800"/>
-    <Container/>
+  <main ref="mainWindow">
+    <Header class="" ref="header" @click="toggelHeaderClass"/>
+    <Container  />
   </main>
 </template>
 
@@ -9,6 +9,21 @@
 import Header from "@/components/Header";
 import Container from "@/components/Container";
 export default {
+  name:'landingPage',
+  methods:{
+      toggelHeaderClass(){
+        // let navbar = this.$refs['navbar'];
+        // if(window.scrollY> 0){
+        //   navbar.classList.add('bg-red-500')
+        //   console.log('hello')
+        // }else{
+        //   console.log('no way');
+        // }
+
+        // console.log(this.$refs['mainWindow']);
+        console.log('test scroll');
+      }
+  },
   components: {
     Header,
     Container
